bd877c537ac38f8334aca670009e6ade55c401b8
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-28 13:37:48 -0800

    epty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,6 @@
 #include "binary_trees.h"
+
+
 /**
  * binary_tree_insert_left - the function pointer 
  * @parent: pointer node that point to the root of the node
@@ -8,13 +10,13 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = malloc(sizeof(binary_tree_t))
-	
+
 	if (root == NULL)
-		return (NULL)
-	
+		return (NULL);
+
 	root->n = value;
 	root->parent = parent;
 	root->left = NULL;
 	root->right = NULL;
-	return (root)
+	return (root);
 }
