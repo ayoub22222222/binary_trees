2c0bc4654f353c5006df26e2c9027b2173bb6f77
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-28 15:45:40 -0800

    the right one

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - the function pointer
+ * binary_tree_node - the function pointer
  * @parent: pointer node that point to the root of the node
  * @value: the value of the node
  * Return: return binary data type
 */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = malloc(sizeof(binary_tree_t));
 
